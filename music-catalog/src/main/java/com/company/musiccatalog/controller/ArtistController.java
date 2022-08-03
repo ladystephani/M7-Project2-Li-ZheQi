689f0461e5e0718b2ac9a534758e3dde5d8cce72
Lady Gaga <jessiezheqi.li@gmail.com>
2022-08-03 11:08:35 -0700

    Add routes for Artist

--- music-catalog/src/main/java/com/company/musiccatalog/controller/ArtistController.java
+++ music-catalog/src/main/java/com/company/musiccatalog/controller/ArtistController.java
@@ -1,10 +1,15 @@
 package com.company.musiccatalog.controller;
 
+import com.company.musiccatalog.model.Artist;
 import com.company.musiccatalog.service.CatalogService;
 import com.company.musiccatalog.viewModel.ArtistViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 @RequestMapping("/artist")
@@ -14,9 +19,40 @@ public class ArtistController {
 
     @PostMapping
     @ResponseStatus(HttpStatus.CREATED)
-    public ArtistViewModel createArtist(@RequestBody ArtistViewModel artistViewModel) {
+    public ArtistViewModel createArtist(@RequestBody @Valid ArtistViewModel artistViewModel) {
         artistViewModel = service.createArtist(artistViewModel);
         return artistViewModel;
     }
 
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<ArtistViewModel> getAllArtists() {
+        List<ArtistViewModel> artistViewModelList = service.getAllArtists();
+        return artistViewModelList;
+    }
+
+    @GetMapping("/{id}")
+    @ResponseStatus(HttpStatus.OK)
+    public ArtistViewModel getArtistById(@PathVariable("id") Integer id) {
+        ArtistViewModel artistViewModel = service.getArtist(id);
+        if (artistViewModel == null) {
+            throw new IllegalArgumentException("Artist could not be retrieved for id " + id);
+        } else {
+            return artistViewModel;
+        }
+    }
+
+    @PutMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void updateArtist(@RequestBody @Valid ArtistViewModel artistViewModel) {
+        if (artistViewModel == null || artistViewModel.getId()<1) {
+            throw new IllegalArgumentException("Check id in view model");
+        } else if (artistViewModel.getId() > 0) {
+            service.updateArtist(artistViewModel);
+        }
+    }
+
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteArtist(@PathVariable("id") Integer id) {service.deleteArtist(id);}
 }
--- music-catalog/src/main/java/com/company/musiccatalog/service/CatalogService.java
+++ music-catalog/src/main/java/com/company/musiccatalog/service/CatalogService.java
@@ -6,6 +6,10 @@ import com.company.musiccatalog.viewModel.ArtistViewModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+
 @Component
 public class CatalogService {
     ArtistRepository artistRepo;
@@ -32,4 +36,54 @@ public class CatalogService {
         return artistViewModel;
     }
 
+    public List<ArtistViewModel> getAllArtists() {
+        //take already seeded artists from database
+        List<Artist> artistList = artistRepo.findAll();
+        List<ArtistViewModel> artistViewModelList = new ArrayList<>();
+
+        if (artistList == null) {
+            return null;
+        } else {
+            //place each artist in vm
+            artistList.stream().forEach(artist -> artistViewModelList.add(buildArtistViewModel(artist)));
+        }
+        return artistViewModelList;
+    }
+
+    public ArtistViewModel getArtist(Integer id) {
+        Optional<Artist> artist = artistRepo.findById(id);
+        if (artist == null) {
+            return null;
+        } else {
+            return buildArtistViewModel(artist.get());
+        }
+    }
+
+    public void updateArtist(ArtistViewModel artistViewModel) {
+        //validate data
+        if (artistViewModel==null) throw new IllegalArgumentException("No artist is passed! Artist object is null!");
+
+        //make sure artist exists. If not, throw exception...
+        if (this.getArtist(artistViewModel.getId()) == null) throw new IllegalArgumentException("No such artist to update");
+
+        Artist artist = new Artist();
+        artist.setId(artistViewModel.getId());
+        artist.setName(artistViewModel.getName());
+        artist.setInstagram(artistViewModel.getInstagram());
+        artist.setTwitter(artistViewModel.getTwitter());
+
+        artistRepo.save(artist);
+    }
+
+    public void deleteArtist(Integer id) {artistRepo.deleteById(id);}
+
+    //helper methods
+    public ArtistViewModel buildArtistViewModel(Artist artist) {
+        ArtistViewModel artistViewModel = new ArtistViewModel();
+        artistViewModel.setId(artist.getId());
+        artistViewModel.setName(artist.getName());
+        artistViewModel.setInstagram(artist.getInstagram());
+        artistViewModel.setTwitter(artist.getTwitter());
+        return artistViewModel;
+    }
 }
